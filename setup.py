10bc3677d165179fc70e61eb00c8ab53156a0101
techthumb1 <robinsonjason761@gmail.com>
2020-07-01 23:24:04 -0500

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="my_lambdata-pt6", # the name that you will install via pip
+    version="1.0",
+    author="Jason Robinson",
+    author_email="robinsonjason761@email.com",
+    description="An brief overview of the conception",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/techthumb1/lambdata-techthumb1.git",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
