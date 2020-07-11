a9be7899d9d70c5f2cfa29a713c68b51cf452182
techthumb1 <robinsonjason761@gmail.com>
2020-07-10 23:06:45 -0500

    update

--- my_lambdata/my_files/beat.py
+++ my_lambdata/my_files/beat.py
@@ -27,16 +27,6 @@ if __name__ == "__main__":
     instrument = Beat("High Kick", "High Snare", "High Hat", "Low Bass")
     print(instrument.kick, instrument.snare)
     instrument.keys()
-    instrument.wash()
+    
 
-    instrument2 = Beat("Tesla", "snare S", 2020, "Blue", 4)
-    instrument2.keys()
-    instrument2.wash()
-
-    truck = Truck("Ford", "F150", 2020, "Blue", 4, bed_size="5x5")
-    truck.keys()
-    truck.wash()
-
-    truck2 = Truck("Tesla", "Cybertuck", 2020, "Blue", 4, bed_size="6x4")
-    truck2.keys()
-    truck2.wash()
+   
