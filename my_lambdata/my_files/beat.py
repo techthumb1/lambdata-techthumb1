b3f5212f4bc26ad22a1bcbd11001a93f3fa6b85d
techthumb1 <robinsonjason761@gmail.com>
2020-07-11 00:44:22 -0500

    Making of a Beat

--- my_lambdata/my_files/beat.py
+++ my_lambdata/my_files/beat.py
@@ -8,25 +8,27 @@ class Beat():
         self.bass = bass
 
     def instrument(self):
-        print("PLAYING THE INSTRUMENT")
+        print("PLAYING THE SOUNDFONT")
 
     def keys(self):
         print("I AM PLAYING THE", self.snare)
 
 
 class Hip_Hop(Beat):
-    def __init__(self, make, snare, hat, bass, trap_bass"):
-        super().__init__(kick, snare, hat, bass, trap_bass)
+    def __init__(self, kick, snare, hat, bass, trap_bass):
+        super().__init__(kick, snare, hat, bass)
         self.trap_bass = trap_bass
 
     def keys(self):
-        print("I AM PLAYING THE", self.snare, "WITH TRAP BASS:", self.trap_bass)
+        print("I AM PLAYING THE", self.snare, "WITH TRAP BASS", self.trap_bass)
 
 
 if __name__ == "__main__":
-    instrument = Beat("High Kick", "High Snare", "High Hat", "Low Bass")
-    print(instrument.kick, instrument.snare)
-    instrument.keys()
-    
+    soundfont = Beat("High Kick", "High Snare", "High Hat", "Low Bass")
+    print(soundfont.kick, soundfont.snare, soundfont.bass)
+    soundfont.keys()
+    soundfont.instrument()
 
-   
+    keys = Hip_Hop("Low Kick", "Mid Snare", "Low Hat", "Mid Bass", "Deep Bass")
+    keys.keys()
+    keys.instrument()
