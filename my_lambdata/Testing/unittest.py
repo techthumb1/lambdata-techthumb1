3bd9dd4683ae00063f0c69374e2f9e3a14565563
techthumb1 <robinsonjason761@gmail.com>
2020-07-16 23:44:56 -0500

    Update Dockerfile

---
+++ my_lambdata/Testing/unittest.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Surgery(unittest.TestCase):
+    def test_scapel(self):
+        
+        prod = Scapel('Test Scapel')
+
+
+    if __name__ == "__main__":
+    unittest.main()
